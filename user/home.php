e3d47acbe8835b129898893fe177058ede30772d
Arif <arif@gmail.com>
2020-03-04 11:11:57 +0700

    no message

--- user/home.php
+++ user/home.php
@@ -72,47 +72,9 @@ height: 350px;
 
 </head>
 <body>
-    <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
-        <div class="container-fluid">
-          
-          </button>
-            <h1 class="d-md-flex" style="font-size: 24px;font-weight: bold;color: #4e73df;">CLEON</h1>
-            <ul class="nav navbar-nav flex-nowrap ml-auto">
-                  
-                </li>
-                <li class="nav-item dropdown no-arrow" role="presentation">
-                    <li class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">
-
-                      <span class="d-md-flex text-gray-600 small">
-                        <i class="typcn typcn-th-large-outline" style="font-size: 32px;color: #2c3e50;">
-                        </i></span></a>
-                        <div
-                            class="dropdown-menu shadow dropdown-menu-right animated--grow-in force-scroll" role="menu" style="width: 215px;"><i class="far fa-user-circle d-flex d-lg-flex justify-content-center justify-content-lg-center" style="font-size: 49px;padding-top: 18px;padding-bottom: 10px;color: #4e73df;"></i>
-                            <h1 class="d-flex d-lg-flex justify-content-center justify-content-lg-center"
-                                style="color: #2c3e50;font-size: 12px;"><?php echo $_SESSION['username'] ?></h1>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" role="presentation" href="#" data-toggle="modal" data-target="#logoutModal" style="color: rgb(78,115,223);font-weight: bold;"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" style="color: #4e73df;"></i>&nbsp;SIGN OUT</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" role="presentation" href="#" data-toggle="modal" data-target="#tambahperusahaan" style="color: rgb(78,115,223);font-weight: bold;"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" style="color: #4e73df;"></i>&nbsp;Tambah Perusahaan</a>
-                            <?php
-                                $query=mysqli_query($config,"SELECT * FROM tb_perusahaan JOIN tb_akun ON tb_perusahaan.id_akun = tb_akun.id_akun WHERE tb_akun.username = '$username' AND status_aktif='diterima'");
-                                while($tempat=mysqli_fetch_array($query)){ ?>
-                            <form action="../aksi/user/beranda.php" method="post">
-                            <div class="dropdown-divider"></div>
-                              <input type="text" name="nmperusahaan" value="<?php echo $tempat['nm_perusahaan'] ?>" hidden>
-                              <!-- <i class="fas fa-user-circle fa-sm fa-fw mr-2 text-gray-400" style="color: #4e73df;font-size: 15px;">&nbsp; -->
-                              <input type="submit" name="submit" value="<?php echo $tempat['nm_perusahaan'] ?>" class="dropdown-item" role="presentation"  style="color: #2c3e50;font-weight: bold;font-size: 15px; padding-left: 18px">
-                              
-                              </i></input>
-                            </form>
-                              <?php } ?>
-                              </div>
-        </li>
-        </li>
-        </ul>
-        </div>
-    </nav>
-    <!-- End: Navbar White -->
+    <!-- Start: Navbar -->
+     <?php include 'menu_user.php'; ?>
+     <!-- End: Navbar White -->
 <br /><br />
  <header class="masthead text-center">
     <div class="overlay"></div>
--- user/menu_user.php
+++ user/menu_user.php
@@ -1,16 +1,39 @@
- <ul class="menu-sidebar">
-      <li>
-        <input type="checkbox" id="sub-one" class="submenu-toggle">        
-        <label class="submenu-label" for="sub-one"><i class="fas fa-user-circle"></i> Akun</label>
-        <div class="arrow right">&#8250;</div>           
-        <ul class="menu-sub">
-          <li class="menu-sub-title">
-            <label class="submenu-label" for="sub-one">Back</label>
-            <div class="arrow left">&#8249;</div>              
-          </li>                          
+<nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+        <div class="container-fluid">
+            <h1 class="d-md-flex" style="font-size: 24px;font-weight: bold;color: #4e73df;">CLEON</h1>
+            <ul class="nav navbar-nav flex-nowrap ml-auto">
+                 
+                </li>
+                <li class="nav-item dropdown no-arrow" role="presentation">
+                    <li class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">
+
+                      <span class="d-md-flex text-gray-600 small">
+                        <i class="typcn typcn-th-large-outline" style="font-size: 32px;color: #2c3e50;">
+                        </i></span></a>
+                        <div
+                            class="dropdown-menu shadow dropdown-menu-right animated--grow-in force-scroll" role="menu" style="width: 215px;"><i class="far fa-user-circle d-flex d-lg-flex justify-content-center justify-content-lg-center" style="font-size: 49px;padding-top: 18px;padding-bottom: 10px;color: #4e73df;"></i>
+                            <h1 class="d-flex d-lg-flex justify-content-center justify-content-lg-center"
+                                style="color: #2c3e50;font-size: 12px;"><?php echo $_SESSION['username'] ?></h1>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" role="presentation" href="#" data-toggle="modal" data-target="#logoutModal" style="color: rgb(78,115,223);font-weight: bold;"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" style="color: #4e73df;"></i>&nbsp;SIGN OUT</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" role="presentation" href="#" data-toggle="modal" data-target="#tambahperusahaan" style="color: rgb(78,115,223);font-weight: bold;"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" style="color: #4e73df;"></i>&nbsp;Tambah Perusahaan</a>
+                            <?php
+                                $query=mysqli_query($config,"SELECT * FROM tb_perusahaan JOIN tb_akun ON tb_perusahaan.id_akun = tb_akun.id_akun WHERE tb_akun.username = '$username' AND status_aktif='diterima'");
+                                while($tempat=mysqli_fetch_array($query)){ ?>
+                            <form action="../aksi/user/beranda.php" method="post">
+                            <div class="dropdown-divider"></div>
+                              <input type="text" name="nmperusahaan" value="<?php echo $tempat['nm_perusahaan'] ?>" hidden>
+                              <!-- <i class="fas fa-user-circle fa-sm fa-fw mr-2 text-gray-400" style="color: #4e73df;font-size: 15px;">&nbsp; -->
+                              <input type="submit" name="submit" value="<?php echo $tempat['nm_perusahaan'] ?>" class="dropdown-item" role="presentation"  style="color: #2c3e50;font-weight: bold;font-size: 15px; padding-left: 18px">
+                                
+                              </input>
+                            </form>
+                              <?php } ?>
+                              </div>
+        </li>
+        </li>
+        
         </ul>
-      </li> 
-      <li><a href="#" data-toggle="modal" data-target="#logoutModal"><i class="fas fa-door-open danger"></i> Log-Out</a></li>                                 
-    </ul> 
-    </li>
-</ul>
+        </div>
+    </nav>
